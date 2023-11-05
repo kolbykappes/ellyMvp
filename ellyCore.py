3fd041f7f9ae0694ba5fc36f0ddde706277bab8c
Kolby Kappes <kolbykappes@gmail.com>
2023-11-05 14:53:22 -0500

    added some styling

--- ellyCore.py
+++ ellyCore.py
@@ -80,11 +80,32 @@ def index():
     try:
         response_obj = json.loads(completion.choices[0].message.function_call.arguments)
         print("OpenAI Response:", response_obj)
-        response_text = response_obj
+
+        # Format the response
+        formatted_response = f"<strong>Job Title:</strong> {response_obj['Job Title']}<br>"
+
+        formatted_response += "<strong>Domain:</strong><br><ul>"
+        for domain in response_obj['Domains']:
+            formatted_response += f"<li>{domain['domain']}: {domain['likelihood_percentage']}%</li>"
+        formatted_response += "</ul>"
+
+        formatted_response += "<strong>Technologies:</strong><br><ul>"
+        for tech in response_obj['Technologies']:
+            formatted_response += f"<li>{tech}</li>"
+        formatted_response += "</ul>"
+
+        formatted_response += "<strong>Skills:</strong><br><ul>"
+        for skill in response_obj['Skills']:
+            formatted_response += f"<li>{skill}</li>"
+        formatted_response += "</ul>"
+
+        response_text = formatted_response
+
     except Exception as e:
         response_text = f"Error: {str(e)}"
 
     return render_template("index.html", response=response_text, title=title, description=description)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
